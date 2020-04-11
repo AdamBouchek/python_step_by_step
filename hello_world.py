385776ddd260983d39f13e46e45fe5f9a588d198
Adam <aa4780338@gmail.com>
2020-04-11 14:41:42 +0100

    my first python prograam on github

---
+++ hello_world.py
@@ -0,0 +1,17 @@
+# Write a program that add two number called a and b then display the result?
+# a = input("enter a ");
+# b = input("enter b ");
+# print(a, "+", b, "=", int(a) + int(b));
+
+# Write a program that subtract two number called a and b then display the result?
+a = input("enter a ");
+b = input("enter b ");
+c = int(a) - int(b);
+print(a, "-", b, "=", c);
+
+# a_number = input("Enter the number? ");
+# print("*****", a_number, "Multiplication table ******");
+# for number in range(1, 11):
+#     print(number, "x", a_number, "=", number * int(a_number));
+
+
